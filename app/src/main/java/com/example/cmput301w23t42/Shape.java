4f3ae0f4f482a0b6a30df28cb41b1bc56b5ec6ee
Isaac-Stamatelakis <97195929+Isaac-Stamatelakis@users.noreply.github.com>
2023-02-02 19:13:34 -0700

    Fixed something idk

--- app/src/main/java/com/example/cmput301w23t42/Shape.java
+++ app/src/main/java/com/example/cmput301w23t42/Shape.java
@@ -3,5 +3,5 @@ package com.example.cmput301w23t42;
 public abstract class Shape {
     private int x;
     private int y;
-    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
+    String shapeColor = "$PUT_YOUR_CHOICE_OF_COLOR";
 }
